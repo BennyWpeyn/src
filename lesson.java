c3ec869e1b128c6dddee7b02f4e5018696aec187
BennyWpeyn <berke.erkan.doan@gmail.com>
2022-12-13 11:15:18 +0300

    hocam bişiler yabdık

--- lesson.java
+++ lesson.java
@@ -1,4 +1,4 @@
-public class Lesson {
+public class Lesson extends Grades {
     int course_cod;
     String course_name;
     int credit;
@@ -6,19 +6,14 @@ public class Lesson {
     String lecturer_info;
     Student students[];
     int population ;                            // the number of the student who takes the lessons;
-
-
+    Grades grade;
+    
     public Lesson(int course_cod, String course_name, int credit){
         this.course_cod = course_cod;
         this.course_name = course_name;
         this.credit = credit;
-
-
+        grade = new Grades();
     }
-
-
-    
-
 
 
 }
