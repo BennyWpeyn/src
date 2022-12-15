577c3b5559250a8e7f1057c895927cb393e5cee2
BennyWpeyn <berke.erkan.doan@gmail.com>
2022-12-15 17:23:12 +0300

    a new page

--- Staff.java
+++
@@ -1,5 +0,0 @@
-public class Staff {
-    int identification_number;
-    String task_info;
-    String working_hours;
-}
--- Undergraduate.java
+++
@@ -1,9 +0,0 @@
-public class Undergraduate extends Student{
-
-    public Undergraduate(int id, String name, int year) {
-        super(id, name, year);
-    }
-    
-
-    
-}
